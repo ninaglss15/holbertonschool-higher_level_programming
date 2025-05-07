dbf13b95c1c21ef116803442a575fb2df71d71b5
ninaglss15 <10526@holbertonstudents.com>
2025-05-07 11:48:32 +0200

    fixe cheker error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,10 @@
 
 import random
 number = random.randint(-10000, 10000)
-pepechiken = abs(number) % 10
-if number < 0:
-    pepechiken = -pepechiken
+pepechiken = (number) % 10
 if pepechiken > 5:
     print(f"Last digit of {pepechiken} and is greater than 5")
 elif pepechiken == 0:
     print(f"Last digit of {pepechiken} and is 0")
 else:
     print(f"Last digit of {pepechiken} and is less than 6 and not 0")
-    
