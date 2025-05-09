b7742316c7cc7fc2ce7be7ae085dea25eadf6cd2
ninaglss15 <10526@holbertonstudents.com>
2025-05-09 21:48:45 +0200

    5-variable_load.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    filtered_names = [name for name in names if not name.startswith("__")]
-    sorted_names = sorted(filtered_names)
 
-    for name in sorted_names:
-        print(name)
+    noms = dir(hidden_4)
+
+    noms_tries = sorted(noms)
+
+    for nom in noms_tries:
+        if not nom.startswith("__"):
+            print(nom)
