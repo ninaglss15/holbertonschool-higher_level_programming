693ef03a929e055c965955cc34e0c68aa4d8b81d
ninaglss15 <10526@holbertonstudents.com>
2025-05-09 21:24:40 +0200

    last exercice

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    filtered_names = [name for name in names if not name.startswith("__")]
+    sorted_names = sorted(filtered_names)
+
+    for name in sorted_names:
+        print(name)
