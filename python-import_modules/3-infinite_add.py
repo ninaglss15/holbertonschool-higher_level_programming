327a4a35b81d6e1393d0a2eba94bcb3479b856ea
ninaglss15 <10526@holbertonstudents.com>
2025-05-09 11:16:48 +0200

    error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+import sys
 
+total = 0
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print(total)
