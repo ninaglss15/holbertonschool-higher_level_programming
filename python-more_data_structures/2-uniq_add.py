37cf86be52724ef1e98e15581747ddadab7f34dc
ninaglss15 <10526@holbertonstudents.com>
2025-05-13 18:40:31 +0200

    second project first test

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -6,5 +6,3 @@ def uniq_add(my_list=[]):
     for i in uniq:
         views += i
     return views
-
-
--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for n in set_2:
-            if i == n:
-                return i
+    return (set_1 & set_2)
