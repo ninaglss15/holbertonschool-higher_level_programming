9f785f43ca4431f8f01ef7124a783d48ef73aa46
ninaglss15 <10526@holbertonstudents.com>
2025-05-21 09:55:27 +0200

    fix error for pycodestyle et i add the doc

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+Module rectangle
+Ce module définit une classe Rectangle vide pour représenter un rectangle.
+"""
+
 
 class Rectangle:
-        pass
+    """
+    Classe Rectangle vide.
+
+    Cette classe pourra être étendue pour représenter un rectangle avec des
+    attributs comme la largeur, la hauteur, et des méthodes associées.
+    """
+    pass
