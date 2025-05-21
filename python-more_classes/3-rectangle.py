512516ff3d3ae61696eb4c7b3426d0f2baa74ea2
ninaglss15 <10526@holbertonstudents.com>
2025-05-21 11:17:23 +0200

    exo 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Module 3-rectangle
+Définit une classe Rectangle avec largeur, hauteur, aire.
+"""
+
+
+class Rectangle:
+    """Classe qui représente un rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialise un rectangle avec une largeur et une hauteur."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter pour la largeur."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter pour la largeur avec vérifications."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter pour la hauteur."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter pour la hauteur avec vérifications."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Retourne l’aire du rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Retourne le périmètre du rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Affiche le rectangle avec des #."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join("#" * self.width for _ in range(self.height))
