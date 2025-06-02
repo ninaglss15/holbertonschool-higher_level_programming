afbef8bd01730b14cce0c9ef3906f1cfbc10c5cd
ninaglss15 <10526@holbertonstudents.com>
2025-06-02 23:52:38 +0200

    ajout des comments

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a Student class with serializable attributes.
+"""
 
 
 class Student:
+    """
+    Represents a student with first name, last name and age.
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
