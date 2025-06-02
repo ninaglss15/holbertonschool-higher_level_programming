89736eb976c2532176963634be4ea957b9b51156
ninaglss15 <10526@holbertonstudents.com>
2025-06-02 16:01:37 +0200

    add exo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a function that reads a text file and prints its content.
+"""
+
 
 def read_file(filename="my_file_0.txt"):
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+    """
     with open(filename, "r", encoding="utf-8") as fichier:
         print(fichier.read(), end="")
