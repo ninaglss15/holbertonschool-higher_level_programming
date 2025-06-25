9541d6a922f054932ba4a8526ff3620ce70c36f2
ninaglss15 <10526@holbertonstudents.com>
2025-06-25 10:05:22 +0200

    Add SQL scripts for various database operations and updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,10 @@
+-- script that creates a table second_table in the database hbtn_0c_0
+
 CREATE table IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256)
     score INT
-),
+);
 
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10)
