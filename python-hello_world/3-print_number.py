204e66808aea6d2609898506e15e9d1feb43b696
ninaglss15 <10526@holbertonstudents.com>
2025-09-01 16:25:21 +0200

    Refactor print statements for consistency and clarity across multiple scripts

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,2 +1,3 @@
 #!/usr/bin/pyhton3
-print (f"{98} Battery street")
+number = 98
+print (f"{number} Battery street")
--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(f"float: {3.14:.2f}")
+number = 3.14159
+print(f"float: {number:.2f}")
