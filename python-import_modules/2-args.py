28a5152f51e4e5f4a1933e112299bc1bd7aa4503
ninaglss15 <10526@holbertonstudents.com>
2025-09-04 15:36:21 +0200

    change point

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv) - 1
     if argc == 0:
-        print("0 arguments:")
+        print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
     else:
