dee94d9ec101a01df133fab491835a3521f80d2e
ninaglss15 <10526@holbertonstudents.com>
2025-09-04 10:18:05 +0200

    Refactor variable names for clarity in argument counting script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,13 +3,13 @@
 if __name__ == "__main__":
 
     import sys
-    argc = sys.argv
-    argc = len(sys.argv) - 1
+    argv = sys.argv
+    argc = len(argv) - 1
     if argc == 0:
         print("0 arguments")
     elif argc == 1:
         print("1 arguents")
     else:
         print("{} arguments:" .format(argc))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
