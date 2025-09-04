d7ca85959cff59265b6a39581c17e7a9ae640e4b
ninaglss15 <10526@holbertonstudents.com>
2025-09-04 14:12:13 +0200

    Fix syntax error in hidden discovery script to correctly filter names

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    noms = dir(hidden_4)
+    noms_tries = sorted(noms)
+    for nom in noms_tries:
+        if not nom stort switch('_'):
+            print(nom)
