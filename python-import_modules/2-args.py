7224aed51b17435008817f4cedbc21d78aacd209
ninaglss15 <10526@holbertonstudents.com>
2025-09-04 10:18:53 +0200

    Fix print statement formatting in argument counting script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv) - 1
     if argc == 0:
-        print("0 arguments")
+        print("0 arguments:")
     elif argc == 1:
-        print("1 arguents")
+        print("1 arguents:")
     else:
         print("{} arguments:" .format(argc))
     for i in range(1, len(argv)):
