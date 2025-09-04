540e632621cd762379df839f2ac59960d00a9a3b
ninaglss15 <10526@holbertonstudents.com>
2025-09-04 15:35:14 +0200

    change argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     argc = len(argv) - 1
-    if argc == 1:
+    if argc == 0:
         print("0 arguments:")
-    elif argc == 2:
-        print("1 arguents:")
+    elif argc == 1:
+        print("1 argument:")
     else:
         print("{} arguments:" .format(argc))
     for i in range(1, len(argv)):
