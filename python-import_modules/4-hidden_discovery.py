d24b6547204773a414e6b5eeb5192a4077a9b49d
ninaglss15 <10526@holbertonstudents.com>
2025-09-05 09:24:23 +0200

    fix some error for 4e task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
+
     noms = dir(hidden_4)
+
     noms_tries = sorted(noms)
+
     for nom in noms_tries:
-        if not nom stort switch('_'):
+        if not nom.startswith("__"):
             print(nom)
