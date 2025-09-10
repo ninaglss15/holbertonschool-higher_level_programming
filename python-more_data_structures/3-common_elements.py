c8d5b77508b923489486044c4b4640638319eed3
ninaglss15 <10526@holbertonstudents.com>
2025-09-10 10:13:15 +0200

    fix error for exo 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for a in set_2:
-            if i == a:
-                return a
+    common_set = set_1 & set_2
+    return common_set
