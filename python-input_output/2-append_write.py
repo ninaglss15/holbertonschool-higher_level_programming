fe233017c46fb81096019d299904d64b6be5be83
ninaglss15 <10526@holbertonstudents.com>
2025-10-01 12:08:29 +0200

    push

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+Module that contains a function to read a text file
+"""
 
 
 def append_write(filename="", text=""):
-    """Append text to a file (UTF-8) and return number of characters written."""
+    """Append text to a file (UTF-8) and return number of characters."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
