0e5e436673001187013b6c82dde1afef233ca44e
jumpcc693 <tvop1218@gmail.com>
2025-10-24 17:39:23 +0200

    fix error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-This module makes a query to the database hbtn_0e_0_usa
-"""
+""" Module to list all states from a database """
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    try:
-        # Check if all arguments are provided
-        if len(sys.argv) != 4:
-            print("Usage: {} username password database".format(sys.argv[0]))
-            sys.exit(1)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
+    for row in rows:
+        print(row)
 
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        sys.exit(1)
+    cursor.close()
+    db.close()
