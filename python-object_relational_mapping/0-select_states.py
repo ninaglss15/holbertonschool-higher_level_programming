3f62308e17d8173aa72f025694170448a6b38fd6
jumpcc693 <tvop1218@gmail.com>
2025-10-24 17:36:24 +0200

    fix error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""Script qui affiche tous les États de la table"""
-
+"""
+This module makes a query to the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    try:
+        # Check if all arguments are provided
+        if len(sys.argv) != 4:
+            print("Usage: {} username password database".format(sys.argv[0]))
+            sys.exit(1)
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    results = cursor.fetchall()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
-    for row in results:
-        print(row)
-
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-from sys import argv
+'''lists all City objects from the database hbtn_0e_14_usa'''
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(City, State).join(State).order_by(City.id).all()
-    for city, state in results:
+    query = session.query(State, City).filter(State.id == City.state_id)\
+                   .order_by(City.id)
+
+    for state, city in query.all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
