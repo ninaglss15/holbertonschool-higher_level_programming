68208e50dd81d457213c9d333efd8832d4ae4a0f
ninaglss15 <10526@holbertonstudents.com>
2025-11-14 11:55:25 +0100

    refactor: clean up comments and improve code readability in task_01_jinja.py

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-Task 1: Creating a Basic HTML Template in Flask with reusable header and footer
-"""
+""" flask app with jinja"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
+    """Home page route."""
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
+    """About page route."""
     return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
+    """Contact page route."""
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
